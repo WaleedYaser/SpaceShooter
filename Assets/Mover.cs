eb2f99ffd84cce8aa7f3013b2d11279466aa4d86
Waleed Yaser <waleedyaser95@gmail.com>
2018-04-22 23:56:16 +0200

    shooting behaviour

---
+++ Assets/Mover.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+	public float speed = 20;
+
+	private void Start()
+	{
+		GetComponent<Rigidbody> ().velocity = transform.forward * speed;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundaries
+{
+	public float minX, maxX, minZ, maxZ;
+}
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 10;
+	public float tiltFactor = 4;
+	public Boundaries boundaries;
+
+	public GameObject boltPrefab;
+	public Transform shotSpawn;
+
+	public float fireRate;
+	private float nextTime;
+
+	private Rigidbody myRigidBody;
+	private float horizontalVelocity, verticalVelocity;
+	private Vector3 shipVelocity;
+
+	private void Start()
+	{
+		myRigidBody = GetComponent<Rigidbody> ();
+	}
+
+	private void Update()
+	{
+		if (Input.GetButton ("Fire1") && Time.time > nextTime) {
+			nextTime = Time.time + fireRate;
+			GameObject copy = Instantiate (boltPrefab, shotSpawn.position, shotSpawn.rotation);
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		// Get input from the user
+		horizontalVelocity = Input.GetAxis ("Horizontal");
+		verticalVelocity = Input.GetAxis ("Vertical");
+
+		// Update velocity
+		shipVelocity = new Vector3 (horizontalVelocity, 0.0f, verticalVelocity) * speed;
+		myRigidBody.velocity = shipVelocity;
+
+		// Add some tilt
+		myRigidBody.rotation = Quaternion.Euler(0.0f, 0.0f, myRigidBody.velocity.x * -tiltFactor);
+
+		// Clamp position to the play area
+		myRigidBody.position = new Vector3 (
+			Mathf.Clamp(myRigidBody.position.x, boundaries.minX, boundaries.maxX),
+			0.0f,
+			Mathf.Clamp(myRigidBody.position.z, boundaries.minZ, boundaries.maxZ)
+		);
+	}
+}
